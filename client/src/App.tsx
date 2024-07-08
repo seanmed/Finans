3649e235c29ebb36704011c7077a5bad6662cb38
seanmed <mednikovsean@gmail.com>
2024-07-07 22:09:28 -0400

    linear regression model applied

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,12 @@
-import { Box, CssBaseline, ThemeProvider } from "@mui/material";
+import { Box } from "@mui/material";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 import { createTheme } from "@mui/material/styles";
 import { useMemo } from "react";
-import { themeSettings } from "./theme";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { themeSettings } from "./theme";
 import Navbar from "@/scenes/navbar";
-import Dashboard from "@/scenes/dashboard"
+import Dashboard from "@/scenes/dashboard";
+import Predictions from "@/scenes/predictions";
 
 function App() {
   const theme = useMemo(() => createTheme(themeSettings), []);
@@ -13,17 +15,17 @@ function App() {
       <BrowserRouter>
         <ThemeProvider theme={theme}>
           <CssBaseline />
-            <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
-              <Navbar/>
-              <Routes>
-                <Route path="/" element={<Dashboard />} />
-                <Route path="/predictions" element={<div>predictins page</div>} />
-              </Routes>             
-            </Box>
-        </ThemeProvider>  
+          <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/predictions" element={<Predictions />} />
+            </Routes>
+          </Box>
+        </ThemeProvider>
       </BrowserRouter>
     </div>
-  )
+  );
 }
 
 export default App;
